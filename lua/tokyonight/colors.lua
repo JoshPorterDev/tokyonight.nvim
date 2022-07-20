15295f1263cbe0cef119e79e4b80596de5a90b69
Josh Porter <joshporterdev@proton.me>
2022-07-19 23:27:48 -0500

    Update colors.lua

--- lua/tokyonight/colors.lua
+++ lua/tokyonight/colors.lua
@@ -78,7 +78,7 @@ function M.setup(config)
   colors.bg_sidebar = (config.transparentSidebar and colors.none) or config.darkSidebar and colors.bg_dark or colors.bg
   colors.bg_float = config.darkFloat and colors.bg_dark or colors.bg
 
-  colors.bg_visual = util.darken(colors.blue0, 0.7)
+  colors.bg_visual = util.darken(colors.blue7, 0.7)
   colors.bg_search = "#0f1010"
   colors.fg_sidebar = colors.fg_dark
 
